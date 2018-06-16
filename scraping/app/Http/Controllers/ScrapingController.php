bdc2158f0e4eeb268ec5ff133a14b7729f674a98
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-06-16 19:38:12 +0900

    スクレイピング処理の更新

--- scraping/app/Http/Controllers/ScrapingController.php
+++ scraping/app/Http/Controllers/ScrapingController.php
@@ -7,24 +7,23 @@ use Goutte\Client;
 
 class ScrapingController extends Controller
 {
-    private $talks = [];
-
     public function index()
     {
         $client = new Client();
         $crawler = $client->request('GET', "https://www.ted.com/talks");
 
-        $crawler->filter('.talk-link')->each(function ($node) {
+        $talks = [];
+        $crawler->filter('.talk-link')->each(function ($node) use (&$talks) {
             $val['speaker'] = $node->filter('.talk-link__speaker')->text();
             $val['title'] = $node->filter('.media__message .ga-link')->text();
             $val['img'] = $node->filter('.thumb__image')->attr('src');
             $val['url'] = $node->filter('a.ga-link')->attr('href');
 
-            $this->talks[] = $val;
+            $talks = $val;
         });
 
         return view('talks.index', [
-            'talks' => $this->talks,
+            'talks' => $talks,
         ]);
     }
 
