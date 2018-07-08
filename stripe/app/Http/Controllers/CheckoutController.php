f24e078bca2c22c325394c5484333afb703a14b0
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-07-08 11:05:15 +0900

    use stripe-php for payment

--- stripe/app/Http/Controllers/CheckoutController.php
+++ stripe/app/Http/Controllers/CheckoutController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\Customer;
+use Stripe\Charge;
 
 class CheckoutController extends Controller
 {
     public function charge(Request $request)
     {
-        dd($request->all());
+        try {
+            // Stripeパッケージを初期化
+            Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+            $customer = Customer::create(array(
+                'email' => $request->stripeEmail,
+                'source' => $request->stripeToken
+            ));
+
+            $charge = Charge::create(array(
+                'customer' => $customer->id,
+                'amount' => 1999,
+                'currency' => 'usd'
+            ));
+
+            return 'Charge successful, you get the course!';
+        } catch (\Exception $ex) {
+            return $ex->getMessage();
+        }
     }
 }
